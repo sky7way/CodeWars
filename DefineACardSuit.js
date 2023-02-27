fe9353498d3ac5b6c9ffb0de4a55d69fbd131048
Aleksandr <sky7way@yandex.ru>
2023-02-27 09:49:41 +0300

    Century From Year

---
+++ DefineACardSuit.js
@@ -0,0 +1,11 @@
+function defineSuit(card) {
+    if(card.slice(-1) === '♣'){
+      return 'clubs'
+    }else if(card.slice(-1) === '♦'){
+      return 'diamonds'
+    }else if(card.slice(-1) === '♥'){
+      return 'hearts'
+    } else if(card.slice(-1) === '♠'){
+    return 'spades' 
+  }
+  }
