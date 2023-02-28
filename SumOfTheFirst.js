ee057dac93ed71cec6b34834fe00097aff51b387
Aleksandr <sky7way@yandex.ru>
2023-02-28 23:00:06 +0300

    Sum of the first nth term of Series v2

--- SumOfTheFirst.js
+++ SumOfTheFirst.js
@@ -7,3 +7,13 @@ function SeriesSum(n){
     }
     return result.toFixed(2)
   }
+
+  // v2
+
+  function SeriesSum(n) {
+    let count = 0
+    for (i = 0; i < n; i++) {
+      count += 1 / (1 + i * 3)
+    }
+    return count.toFixed(2)
+  }
