4c3721af01332611abbb935340943a97fc0b611b
Aleksandr <sky7way@yandex.ru>
2023-03-08 13:42:39 +0300

    Square(n) Sum v2

--- Square(n)Sum.js
+++ Square(n)Sum.js
@@ -1,1 +1,3 @@
-const squareSum = (numbers) =>  (numbers.map(i => i**2)).reduce((acc, cur) => cur + acc, 0);
+// const squareSum = (numbers) =>  (numbers.map(i => i**2)).reduce((acc, cur) => cur + acc, 0);
+
+const squareSum = (numbers) => numbers.reduce((acc, cur) => cur**2 + acc, 0);
