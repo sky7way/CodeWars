40b0411553886abe6fc5e18e0ef8a0cc298b06ed
Aleksandr <sky7way@yandex.ru>
2023-03-08 14:10:44 +0300

    Price of Mangoes v2

--- PriceOfMangoes.js
+++ PriceOfMangoes.js
@@ -1,6 +1,11 @@
+// function mango(quantity, price){
+//     const remainder = quantity % 3        
+//     const discount_mango = (quantity - remainder) * 2/3 * price        
+//     const total_cost = discount_mango + (remainder * price)    
+//     return total_cost
+// };
+
 function mango(quantity, price){
-    const remainder = quantity % 3        
-    const discount_mango = (quantity - remainder) * 2/3 * price        
-    const total_cost = discount_mango + (remainder * price)    
-    return total_cost
-};
+    const count = Math.floor(quantity/3)
+    return (quantity - count) * price
+  };
