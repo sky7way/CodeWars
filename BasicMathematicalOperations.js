e0a373362022afcb99c32a0f7dc2b7634c040fe2
Aleksandr <sky7way@yandex.ru>
2023-03-08 11:52:00 +0300

    Basic Mathematical Operations v2

--- BasicMathematicalOperations.js
+++ BasicMathematicalOperations.js
@@ -1,13 +1,24 @@
+// function basicOp(operation, value1, value2)
+// {
+//   switch(operation){
+//     case '+': 
+//     return value1 + value2;
+//     case '-': 
+//     return value1 - value2;
+//     case '*': 
+//     return value1 * value2;
+//     case '/': 
+//     return value1 / value2;
+//   }
+// }
+
 function basicOp(operation, value1, value2)
 {
-  switch(operation){
-    case '+': 
-    return value1 + value2;
-    case '-': 
-    return value1 - value2;
-    case '*': 
-    return value1 * value2;
-    case '/': 
-    return value1 / value2;
+  const result = { 
+        '+': value1 + value2,
+        '-': value1 - value2,
+        '*': value1 * value2,
+        '/': value1 / value2
   }
+   return result[operation]
 }
