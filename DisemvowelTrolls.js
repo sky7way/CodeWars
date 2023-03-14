c31ecc0efeb8fd62ce97a2d5bf1341735e061184
Aleksandr <sky7way@yandex.ru>
2023-03-14 14:52:54 +0300

    Disemvowel Trolls v2

--- DisemvowelTrolls.js
+++ DisemvowelTrolls.js
@@ -1,5 +1,9 @@
-const disemvowel = (str) => {
-    const arr = str.split('')
-    return arr.filter(el => !'aeiou'.includes(el.toLowerCase())).join('')
-  };
-  
+// const disemvowel = (str) => {
+//     const arr = str.split('')
+//     return arr.filter(el => !'aeiou'.includes(el.toLowerCase())).join('')
+//   };
+
+
+///////////////////////////////////////////////////////////////////
+
+const disemvowel = (str) => str.replace(/[aeiou]/gi, '');
