1aed35f912ee8a1d13cef48770cace30d4044d71
Aleksandr <sky7way@yandex.ru>
2023-05-12 20:11:02 +0300

    Persistent Bugger v2

--- PersistentBugger.js
+++ PersistentBugger.js
@@ -1,1 +1,14 @@
 const persistence = num => num > 9 ? 1 + persistence([...`${num}`].reduce((pre, val) => pre * val)) : 0;
+
+////////////////////////////////////
+
+function persistence(num) {
+    let amount = 0;
+    
+    while(num > 9) {
+       num = [...num + ''].reduce((a, b) => a * b);
+       amount++;
+    }
+    
+    return amount;
+  }
