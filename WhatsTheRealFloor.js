49ffa7d71006293ab02be0ea48c70aa1b159a07a
Aleksandr <sky7way@yandex.ru>
2023-05-15 09:54:01 +0300

    What's the real floor? v2

--- WhatsTheRealFloor.js
+++ WhatsTheRealFloor.js
@@ -1,11 +1,15 @@
-function getRealFloor(n) {
-    if (n === 0 || n === 1) {
-        return 0;
-    } else if (n > 1 && n < 13) {
-        return n - 1;
-    } else if (n > 13) {
-        return n - 2
-    } else if (n < 0) {
-        return -Math.abs(n);
-    }
-}
+// function getRealFloor(n) {
+//     if (n === 0 || n === 1) {
+//         return 0;
+//     } else if (n > 1 && n < 13) {
+//         return n - 1;
+//     } else if (n > 13) {
+//         return n - 2
+//     } else if (n < 0) {
+//         return -Math.abs(n);
+//     }
+// }
+
+////////////////////
+
+const getRealFloor = n => n > 13 ? n - 2 : n > 0 ? n - 1 : n;
