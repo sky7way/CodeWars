a2a41fd088f39cd83e8d07b6be14bce9b8759d3e
Aleksandr <sky7way@yandex.ru>
2023-05-15 09:35:12 +0300

    Short Long Short v2

--- ShortLongShort.js
+++ ShortLongShort.js
@@ -1,1 +1,11 @@
-const solution = (a, b) => a.length < b.length ? a + b + a : b + a + b;
+// const solution = (a, b) => a.length < b.length ? a + b + a : b + a + b;
+
+///////////////////////
+
+function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
+}
