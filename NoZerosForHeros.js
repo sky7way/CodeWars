35ae02a791d85b5d6d8cd5c3ba6b451d80e7b792
Aleksandr <sky7way@yandex.ru>
2023-05-16 16:28:55 +0300

    No zeros for heros  v2

--- NoZerosForHeros.js
+++ NoZerosForHeros.js
@@ -1,4 +1,9 @@
-const noBoringZeros = n => {
-    while(n%10==0 && n!=0){n/=10;}
-    return n;
-  }
+// const noBoringZeros = n => {
+//     while(n%10==0 && n!=0){n/=10;}
+//     return n;
+//   }
+
+/////////////////////
+
+const noBoringZeros = n => +`${n}`.replace(/0*$/, "");
+  
